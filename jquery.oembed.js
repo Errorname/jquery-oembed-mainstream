993a8a4f3bbef0fd3605239b1606caa8575b095f
Thibaud Courtoison <do.not.press.enter@gmail.com>
2016-08-02 18:14:59 +0200

    Rework most of the code

---
+++ jquery.oembed.js
@@ -0,0 +1,190 @@
+/*!
+ * jQuery oEmbed plugin with mainstream providers
+ *
+ * Copyright (c) 2016 Thibaud Courtoison
+ * Licensed under MIT License
+ *
+ * Original Author: Richard Chamorro
+ * First fork: Andrew Mee
+ */
+
+(function($) {
+
+	var defaults = {
+		providers: '*',
+		onError: function(e) {
+			console.log('OEmbed error:');
+			console.log(e);
+		}
+	};
+
+	$.fn.oembed = function () {
+
+		// Enable several ways to call function
+		var url,options;
+		if ($.type(arguments[0]) === 'string') {
+			url = arguments[0];
+			options = arguments[1];
+		} else {
+			url = null;
+			options = arguments[0];
+		}
+
+		var settings = $.extend(true, {}, $.fn.oembed.defaults, options);
+
+		this.ret = this.each(function() {
+
+			var container = $(this);
+			var resourceURL = (url && (!url.indexOf('http://') || !url.indexOf('https://'))) ? url : container.attr("href");
+
+			if (resourceURL) {
+
+				provider = $.fn.oembed.getOEmbedProvider(resourceURL, settings.providers);
+
+				if (provider) {
+
+					provider.getEmbedCode(resourceURL, settings, function(embedCode) {
+			
+						$('<div>')
+							.addClass('oembed-container')
+							.append(embedCode)
+							.insertAfter(container);
+						container.remove();
+
+					});
+
+				} else {
+					settings.onError({type: 'provider not authorized', message: 'The provider could not be found in the authorized list'});
+				}
+
+			} else {
+				settings.onError({type: 'URL not found', message: 'The URL could not be found'});
+			}
+
+			//return container;
+		});
+	};
+
+	$.fn.oembed.defaults = defaults;
+
+	$.fn.oembed.getOEmbedProvider = function(url, providers) {
+
+		var providerList = $.fn.oembed.providers;
+
+		// Limit list of authorized providers
+		if (providers && providers != '*')
+			providerList = providers;
+
+		for (key in providerList) {
+			var provider = $.fn.oembed.providers[key] || $.fn.oembed.providers[providerList[key]];
+
+			for (i in provider.urls) {
+				var regExp = new RegExp(provider.urls[i], 'i');
+
+				if (url.match(regExp) !== null)
+					return provider;
+			}
+		}
+
+		return null;
+	};
+
+	$.fn.oembed.queryAPI = function(url, provider) {
+
+		var requestUrl = provider.getRequestUrl(url);
+
+	};
+
+})(jQuery);
+
+
+// Constructor function for OEmbedProvider Class
+$.fn.oembed.OEmbedProvider = function(settings) {
+
+	for (var property in (settings || {})) {
+		this[property] = settings[property];
+	}
+
+	this.name = this.name || 'error';
+	this.type = this.type || 'rich'; // 'photo', 'video', 'link', 'rich', null
+	this.urls = this.urls || []; // url schemes
+	this.api = this.api || 'http://error.error'; // api endpoint
+	this.format = this.format || 'json';
+
+};
+
+$.fn.oembed.OEmbedProvider.prototype.getRequestUrl = function(resourceURL) {
+
+	var url = this.api;
+
+	url += (url.indexOf("?") <= 0) ? "?" : "&";
+
+	url += "format="+this.format + "&url=" + escape(resourceURL);
+
+	if (this.dataType && this.dataType == 'jsonp')
+		url += '&'+(this.callbackParameter ? this.callbackParameter : "callback")+'=?';
+
+	return url;
+
+};
+
+$.fn.oembed.OEmbedProvider.prototype.getEmbedCode = function(resourceURL, settings, callback) {
+	
+	var requestURL = this.getRequestUrl(resourceURL);
+	var self = this;
+
+	if (this.yql) {
+
+		$.ajax({
+			url: 'http://query.yahooapis.com/v1/public/yql',
+			data: {
+				q: "select * from json where url ='"+requestURL+"'",
+				format: "json"
+			},
+			dataType: 'jsonp',
+			success: function(data) {
+
+				if (data.query.count > 0) {
+					callback(self.getCode(self.yql(data)));
+				} else {
+					settings.onError({type:'yql null', message:'YQL request return an empty result'});
+				}
+
+			},
+			error: function(e) {
+				settings.onError({type:'ajax error', message:'Error during ajax request', error: e});
+			}
+		});
+
+	} else {
+
+		$.ajax({
+			url: requestURL,
+			dataType: 'json',
+			success: function(data) {
+
+				callback(self.getCode(data));
+
+			},
+			error: function(e) {
+				settings.onError({type:'ajax error', message:'Error during ajax request', error: e});
+			}
+		});
+
+	}
+};
+
+$.fn.oembed.OEmbedProvider.prototype.getCode = function(data) {
+
+	if ($.isFunction(this.build_code))
+		this.build_code(data);
+	else {
+
+		if (data.html) {
+			return data.html;
+		} else {
+				settings.onError({type:'unknown instructions', message:"The script is unable to know what to do with the returned data"});
+		}
+	}
+
+};
---
+++ providers.oembed.js
@@ -0,0 +1,90 @@
+
+/* List of mainstream providers */
+
+$.fn.oembed.providers = {
+
+	'dailymotion': new $.fn.oembed.OEmbedProvider({
+					name: 'dailymotion',
+					type: 'video',
+					urls: [
+						"dailymotion\\.com/.+"
+					],
+					api: "//www.dailymotion.com/services/oembed",
+					dataType: 'jsonp'
+				}),
+
+	'flickr': new $.fn.oembed.OEmbedProvider({
+					name: 'flickr',
+					type: 'photo',
+					urls: [
+						"flickr\\.com/photos/.+"
+					],
+					api: "//www.flickr.com/services/oembed",
+					dataType: 'jsonp',
+					callbackParameter: 'jsoncallback'
+				}),
+
+	'imgur': new $.fn.oembed.OEmbedProvider({
+					name: 'imgur',
+					type: 'photo',
+					urls: [
+						"imgur\\.com/gallery/.+"
+					],
+					api: "//api.imgur.com/oembed"
+				}),
+
+	'instagram': new $.fn.oembed.OEmbedProvider({
+					name: 'instagram',
+					type: 'photo',
+					urls: [
+						"instagr\\.?am(\\.com)?/.+"
+					],
+					api: "//api.instagram.com/oembed",
+					dataType: 'jsonp'
+				}),
+
+	'soundcloud': new $.fn.oembed.OEmbedProvider({
+					name: 'soundcloud',
+					type: 'rich',
+					urls: [
+						"soundcloud.com/.+",
+						"snd.sc/.+"
+					],
+					api: "//soundcloud.com/oembed"
+				}),
+
+	'spotify': new $.fn.oembed.OEmbedProvider({
+					name: 'spotify',
+					type: 'rich',
+					urls: [
+						"open.spotify.com/(track|album|user)/"
+					],
+					api: "//embed.spotify.com/oembed/",
+					dataType: 'jsonp'
+				}),
+
+	'vimeo': new $.fn.oembed.OEmbedProvider({
+					name: 'vimeo',
+					type: 'video',
+					urls: [
+						"www\.vimeo\.com\/groups\/.*\/videos\/.*",
+						"www\.vimeo\.com\/.*",
+						"vimeo\.com\/groups\/.*\/videos\/.*",
+						"vimeo\.com\/.*"
+					],
+					api: "//vimeo.com/api/oembed.json"
+				}),
+
+	'youtube': new $.fn.oembed.OEmbedProvider({
+					name: 'youtube',
+					type: 'video',
+					urls: [
+						"youtube\\.com/watch.+v=[\\w-]+&?",
+						"youtu\\.be/[\\w-]+",
+						"youtube.com/embed"
+					],
+					api: "www.youtube.com/oembed",
+					yql: (data) => {return data.query.results.json}
+				}),
+
+};
